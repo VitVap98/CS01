9dc82a6736379e25da10fb0ecd4e7da56af57298
VitVap98 <vitriolicvapor98@gmail.com>
2024-06-15 08:59:49 +0100

    Created the Points types

---
+++ MyTypes.cs
@@ -0,0 +1,42 @@
+namespace MyTypes
+{
+    namespace Points
+    {
+        public abstract class PointAbstract
+        {
+            public abstract double GetMagnitude();
+        }
+
+        public class Point2D : PointAbstract
+        {
+            protected double X = 0;
+            protected double Y = 0;
+            public Point2D(double x, double y){
+                this.X = x;
+                this.Y = y;
+            }
+            public override double GetMagnitude(){
+                return (double)MathF.Sqrt((float)(this.X * this.X + this.Y * this.Y));
+            }
+            public override string ToString()
+            {
+                return $"<{this.X}, {this.Y}>";
+            }
+        }
+
+        public class Point3D : Point2D
+        {
+            private double Z = 0;
+            public Point3D(double x, double y, double z):base(x,y){
+                this.Z = x;
+            }
+            public override double GetMagnitude(){
+                return (double)MathF.Sqrt((float)(this.X * this.X + this.Y * this.Y + this.Z * this.Z));
+            }
+            public override string ToString()
+            {
+                return $"<{this.X}, {this.Y}, {this.Z}>";
+            } 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 using System;
+using MyTypes;
 
 class Program{
     public static void Main(string[] args){
-        Console.WriteLine("Dave");
+        MyTypes.Points.Point2D flatPoint1 = new MyTypes.Points.Point2D(3,4);
+        Console.WriteLine(flatPoint1);
+        Console.WriteLine(flatPoint1.GetMagnitude());
+        MyTypes.Points.Point3D spacePoint1 = new MyTypes.Points.Point3D(1,1,1);
+        Console.WriteLine(spacePoint1);
+        Console.WriteLine(spacePoint1.GetMagnitude());
     }
 }
